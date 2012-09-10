22222d76604442b4c071a015a79bc4924d5b4976
Manish Valechha <manishval@hotmail.com>
2012-09-09 20:01:26 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paisly::Application.config.secret_token = '55bd3e1a121784a49e1d1d847f98642be56a256a21627f9bcc91b586da22a4e1e48977a65837f50f657796cd6b5ccb839c99a4d2cb3c93a3be39a6e1998605d4'
