aebff54fb4debaccd5c0534bf4218f581b477ec7
Manish Valechha <manish@lavabits.com>
2012-09-13 10:00:33 -0400

    Complete login and logout actions and authenticate user on restricted controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :current_user
+
+  private
+  def authenticate
+    if session[:user_id].nil?
+      redirect_to login_path
+    end
+  end
+
+  def current_user
+    current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class DashboardController < ApplicationController
+  before_filter :authenticate
+  
   def index
   end
 end
