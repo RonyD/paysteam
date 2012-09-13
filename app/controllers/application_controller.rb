599980cf3b3c8ea240e3420ad4ed5199bc824da7
Manish Valechha <manish@lavabits.com>
2012-09-13 12:54:45 -0400

    Fix workgroup views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   helper_method :current_user
+  helper_method :current_company
 
   private
   def authenticate
@@ -12,5 +13,9 @@ class ApplicationController < ActionController::Base
 
   def current_user
     current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def current_company
+    current_company ||= current_user.company if current_user
   end
 end
